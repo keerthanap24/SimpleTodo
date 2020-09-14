bb009ef2d115a0e1b65ec05cc36205e7525d173c
keerthanap24 <kpullela24@gmail.com>
2020-09-14 12:07:25 -0700

    dding walkthrough gif"
    i

--- app/src/main/java/com/example/simpletodo/EditActivity.java
+++ app/src/main/java/com/example/simpletodo/EditActivity.java
@@ -1,39 +1,55 @@
 package com.example.simpletodo;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class EditActivity extends AppCompatActivity {
+/**
+ * The activity (screen) for editing to-do items. It receives data from MainActivity and
+ * returns result of the editing back.
+ */
+public class EditItemActivity extends AppCompatActivity {
 
     EditText etItem;
-    Button btnSave;
-
+    Button btnUpdate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit);
+        setContentView(R.layout.activity_edit_item);
+        etItem = findViewById(R.id.etItemText);
+        btnUpdate = findViewById(R.id.btnUpdate);
 
-        etItem = findViewById(R.id.etItem);
-        btnSave = findViewById(R.id.btnSave);
+        etItem.setText(getIntent().getStringExtra(MainActivity.ITEM_TEXT));
+        int position = getIntent().getIntExtra(MainActivity.ITEM_POSITION, 0);
+        // read the data passed from MainActivity by accessing it by Keys from MainActivity
+        etItem.setText(getIntent().getStringExtra(MainActivity.KEY_ITEM_TEXT));
+        position = getIntent().getIntExtra(MainActivity.KEY_ITEM_POSITION, 0);
 
         getSupportActionBar().setTitle("Edit item");
 
-        etItem.setText(getIntent().getStringExtra(MainActivity.KEY_ITEM_TEXT));
-
-        btnSave.setOnClickListener(new View.OnClickListener() {
+        // when user is done editing they click "update" button
+        final int finalPosition = position;
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Create an Intent() that will be container for results
                 Intent intent = new Intent();
-                intent.putExtra(MainActivity.KEY_ITEM_TEXT, etItem.getText().toString());
-                intent.putExtra(MainActivity.KEY_ITEM_POSITION, getIntent().getExtras().getInt(MainActivity.KEY_ITEM_POSITION));
-                setResult(RESULT_OK, intent);
-                finish();
+                intent.putExtra(MainActivity.ITEM_POSITION, finalPosition);
+                intent.putExtra((MainActivity.ITEM_TEXT), etItem.getText().toString());
 
+                // pass results of editing:
+                // notice we are using same Keys from MainActivity, but new, updated Values
+                intent.putExtra(MainActivity.KEY_ITEM_POSITION, finalPosition);
+                intent.putExtra((MainActivity.KEY_ITEM_TEXT), etItem.getText().toString());
+
+                // set result and pass data back
+                setResult(RESULT_OK, intent);
+
+                // finish activity = close this screen and go back
+                finish();
             }
         });
     }
--- app/src/main/java/com/example/simpletodo/MainActivity.java
+++ app/src/main/java/com/example/simpletodo/MainActivity.java
@@ -1,123 +1,150 @@
-package com.example.simpletodo;
-
-import android.content.ClipData;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.FileUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
+import org.apache.commons.io.FileUtils;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
-
 public class MainActivity extends AppCompatActivity {
-
-    public static final String KEY_ITEM_TEXT = "item_text";
-    public static final String KEY_ITEM_POSITION = "item_position";
-    public static final int EDIT_TEXT_CODE = 20;
+    // a numeric code to identify the edit activity
+    public static final int EDIT_REQUEST_CODE = 20;
+    // key used for passing data between activities
+    public static final String ITEM_TEXT = "itemText";
+    public static final String ITEM_POSITION = "itemPosition";
+    public static final String KEY_ITEM_TEXT = "itemText";
+    public static final String KEY_ITEM_POSITION = "itemPosition";
 
     Button btnAdd;
-    EditText etItem;
+    EditText etTodo;
     RecyclerView rvItems;
-    ItemsAdapter itemsAdapter;
     List<String> items;
-
+    ItemsAdapter adapter;
+    // Add RecyclerView AndroidX library to the Gradle build file DONE
+    //Define a model class to use as the data source DONE
+    //Add a RecyclerView to your activity to display the items DONE
+    //Create a custom row layout XML file to visualize the item DONE- we'll use the built in
+    //Create a RecyclerView.Adapter and ViewHolder to render the item
+    //Bind the adapter to the data source to populate the RecyclerView
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnAdd = findViewById(R.id.btnAdd);
-        etItem = findViewById(R.id.etItem);
+        etTodo = findViewById(R.id.etTodo);
         rvItems = findViewById(R.id.rvItems);
 
+        readItems();
         loadItems();
-
-
+        rvItems.setLayoutManager(new LinearLayoutManager(this));
         ItemsAdapter.OnLongClickListener onLongClickListener = new ItemsAdapter.OnLongClickListener() {
-
             @Override
             public void onItemLongClicked(int position) {
+                Log.i("MainActivity", "Long press at position " + position);
                 items.remove(position);
-                itemsAdapter.notifyItemRemoved(position);
-                Toast.makeText(getApplicationContext(), "Item was removed", Toast.LENGTH_SHORT).show();
+                adapter.notifyItemRemoved(position);
+                writeItems();
                 saveItems();
             }
         };
-
         ItemsAdapter.OnClickListener onClickListener = new ItemsAdapter.OnClickListener() {
             @Override
-            public void onItemClicked(int position) {
-                Log.d("MainActivity", "Single click at position" + position);
-                Intent i = new Intent(MainActivity.this, EditActivity.class);
-                i.putExtra(KEY_ITEM_TEXT, items.get(position));
-                i.putExtra(KEY_ITEM_POSITION, position);
-                startActivityForResult(i, EDIT_TEXT_CODE);
+            public void onClickListener(int position) {
+                Log.d("MainActivity", "Singe press at position " + position);
+                // create the new activity
+                Intent intent = new Intent(MainActivity.this, EditItemActivity.class);
+                // pass the data being edited
+                intent.putExtra(ITEM_POSITION, position);
+                intent.putExtra(ITEM_TEXT, items.get(position));
+                // pass the data being edited as key-value pair
+                intent.putExtra(KEY_ITEM_POSITION, position);
+                intent.putExtra(KEY_ITEM_TEXT, items.get(position));
+                // display the activity
+                startActivityForResult(intent, EDIT_REQUEST_CODE);
             }
         };
-        itemsAdapter = new ItemsAdapter(items, onLongClickListener, onClickListener);
-        rvItems.setAdapter(itemsAdapter);
-        rvItems.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new ItemsAdapter(this, items, onLongClickListener, onClickListener);
+        rvItems.setAdapter(adapter);
+
 
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                String todoItem = etItem.getText().toString();
-                items.add(todoItem);
-                itemsAdapter.notifyItemInserted(items.size() - 1);
-                etItem.setText("");
-                Toast.makeText(getApplicationContext(), "Item was added", Toast.LENGTH_SHORT).show();
+            public void onClick(View v) {
+                String itemText = etTodo.getText().toString();
+                if (itemText.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Cannot add an empty item", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                etTodo.setText("");
+                // Add the new item to the list
+                items.add(itemText);
+                adapter.notifyItemInserted(items.size() - 1);
+                Toast.makeText(MainActivity.this, "Added item", Toast.LENGTH_SHORT).show();
+                writeItems();
                 saveItems();
             }
-
         });
-
-
     }
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if (resultCode == RESULT_OK && requestCode == EDIT_TEXT_CODE) {
+        // if the edit activity completed ok
+        if (resultCode == RESULT_OK && requestCode == EDIT_REQUEST_CODE) {
+            // Update the list with the updated item text
+            String itemText = data.getStringExtra(ITEM_TEXT);
+            // extract original position of the edited item
+            int position = data.getIntExtra(ITEM_POSITION, 0);
+            // Retrieve updated item text value using the key for item text
             String itemText = data.getStringExtra(KEY_ITEM_TEXT);
-            int position = data.getExtras().getInt(KEY_ITEM_POSITION);
+            // extract original position of the edited item by requesting value of position key
+            int position = data.getIntExtra(KEY_ITEM_POSITION, 0);
+            // update the model with the new item text at the edited position
             items.set(position, itemText);
-            itemsAdapter.notifyItemChanged(position);
+            // notify the adapter
+            adapter.notifyItemChanged(position);
+            // persist the changes
+            writeItems();
             saveItems();
-            Toast.makeText(getApplicationContext(), "Item updated successfully!", Toast.LENGTH_SHORT).show();
+            // notify the user that the operation completed successfully
+            Toast.makeText(this, "Item updated successfully", Toast.LENGTH_SHORT).show();
         } else {
             Log.w("MainActivity", "Unknown call to onActivityResult");
         }
     }
-
     private File getDataFile() {
         return new File(getFilesDir(), "data.txt");
     }
 
-
-    private void loadItems() {
-        try {
-            items = new ArrayList<>(FileUtils.readLines(getDataFile(), String.valueOf(Charset.defaultCharset())));
-        } catch (IOException e) {
-            Log.e("MainActivity", "Error reading items", e);
-            items = new ArrayList<>();
+    private void readItems() {
+        /**
+         * This function loads items by reading every line in a local data file.
+         */
+        private void loadItems() {
+            try {
+                items = new ArrayList<>(FileUtils.readLines(getDataFile(), Charset.defaultCharset()));
+            } catch (IOException e) {
+                Log.e("MainActivity", "Error reading items", e);
+                items = new ArrayList<>();
+            }
         }
-    }
 
-    private void saveItems() {
-        try {
-            FileUtils.writeLines(getDataFile(), String.valueOf(items));
-        } catch (IOException e) {
-            Log.e("MainActivity", "Error writing items", e);
+        private void writeItems() {
+            /**
+             * This function saves items by writing into a local data file.
+             */
+            private void saveItems() {
+                try {
+                    FileUtils.writeLines(getDataFile(), items);
+                } catch (IOException e) {
+                    Log.e("MainActivity", "Error writing file", e);
+                }
+            }
         }
-    }
-}
